4cfd5d681bf2ddc6072f9abe74c1922d34a2df45
Frantisek Kocun <frantisek.kocun@merchenta.com>
2015-12-10 21:12:09 -0500

    polished working console

--- src/tic_tac_toe/core.cljs
+++ src/tic_tac_toe/core.cljs
@@ -1,44 +1,58 @@
 (ns tic-tac-toe.core
-  (:require [reagent.core :as r] 
-  	        [cljs.reader :as reader]
+  (:require (comment [reagent.core :as r] )
+            [cljs.reader :as reader]
             ))
 
 (enable-console-print!)
 
+; game state
 (defonce default-game-state 
-  { :winner :none
+  { :winner nil
     :game-status :in-progress
     :current-player :cross
     :board {}
     })
 
-(defn solution [x y dx dy] 
-	(take 3 (iterate  #(vector (+ dx (get % 0)) (+ dy (get % 1)))  [x y] ))
-)
+; vector of keys of all possible solutions
+; 3x row
+; 3x column
+; 2x diagonal
+(def solutions 
+  (letfn 
+    [(solution [x y dx dy] 
+      (take 3 (iterate  #(vector (+ dx (get % 0)) (+ dy (get % 1)))  [x y] )))]
+    [
+      (solution 0 0 0 1)
+      (solution 1 0 0 1)
+      (solution 2 0 0 1)
+      (solution 0 0 1 0)
+      (solution 0 1 1 0)
+      (solution 0 2 1 0)
+      (solution 0 0 1 1)
+      (solution 0 2 1 -1)]))
 
-(def solutions [
-	(solution 0 0 0 1)
-	(solution 1 0 0 1)
-	(solution 2 0 0 1)
-	(solution 0 0 1 0)
-	(solution 0 1 1 0)
-	(solution 0 2 1 0)
-	(solution 0 0 1 1)
-	(solution 0 2 1 -1)
-])
-
+; select the values from map and remove nil values
 (defn select-values [m ks] 
-         (reduce #(if-let [v (m %2)] (conj %1 v) %1) [] ks))
+        (remove nil? (map m ks)))
 
-(defn countWithFilter [predicate sequence]
-	(count (filter predicate sequence)
-	)
-)
-
+; checks if player won the game
+; = there is a solution (list positions on the board) for which all 3 values are player's
+; #{player} - creates set with player inside. Sets are alo functions returning the element if it's inside
 (defn did-player-win? [board player]
-	(some #(= 3 %) (map (fn [n] (countWithFilter #(= player %) n)) (map #(select-values board %) solutions)))
+  (some #(= 3 (count (filter #{player} (select-values board %) ))) solutions)
 )
 
+(defn is-board-full? [board]
+  (= 9 (count board))
+)
+
+(def board
+  {
+    [0 0] :cross [0 1] :circle [0 2] :cross
+    [1 0] :cross               [1 2] :circle
+    [2 0] :cross [2 1] :circle
+  }
+)
 
 (defn prompt-player [] (
   js/prompt "Make a move" "[0 0]"))
@@ -53,7 +67,7 @@
 (defn print-winner [game-state] 
   (print 
     (case (:winner game-state)
-      :none "It's a draw!"
+      nil "It's a draw!"
       :cross "Cross won the game!"
       :circle "Circle won the game!"
       )))
@@ -67,23 +81,27 @@
 (defn run-game [game-state]
   (do
     (print-game-status game-state)
-  	(if (= :completed (:game-status game-state))
-    	(print-winner game-state)
-    	(let [turn (prompt-player)
-    		  position (reader/read-string turn)
-    		  current-player (:current-player game-state)
-			  new-game-state (assoc-in game-state [:board position] current-player)
-			  current-player-won (did-player-win? (:board new-game-state) current-player)
-			  ]
-			  (do
-    		 	(print new-game-state)
-    		 	(print "current-player-won " current-player-won)
+    (if (= :completed (:game-status game-state))
+      
+      (print-winner game-state)
 
-    		 	(run-game (update-in new-game-state [:current-player] #(if (= :cross %) :circle :cross) ))
-    		  )
-    	)
+      (let [turn (prompt-player)
+            position (reader/read-string turn)
+            current-player (:current-player game-state)
+            new-board (assoc-in (:board game-state) [position] current-player)
+            current-player-won (did-player-win? new-board current-player)
+            is-board-full (is-board-full? new-board)
+            new-game-state (-> game-state
+              #(if current-player-won (assoc % :winner current-player) %)
+              #(if (or current-player-won is-board-full) (assoc % :game-status :completed) %)
+              #(update-in % [:current-player] #(if (= :cross %) :circle :cross) )
+              #(update-in % [:board] new-board)
+            )
+           ]
+
+          (run-game new-game-state)
+      )
     )
-  	;	(p)
 ))
 
     
