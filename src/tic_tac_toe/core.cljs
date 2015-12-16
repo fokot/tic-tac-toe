592ddeaf0aa7bba21032212de97b57f87a3b8041
Frantisek Kocun <frantisek.kocun@merchenta.com>
2015-12-15 23:01:18 -0500

    new css

--- src/tic_tac_toe/core.cljs
+++ src/tic_tac_toe/core.cljs
@@ -1,6 +1,5 @@
 (ns tic-tac-toe.core
   (:require [reagent.core :as r] 
-            [cljs.reader :as reader]
             ))
 
 (enable-console-print!)
@@ -61,11 +60,10 @@
   [game-state position player]
     (let [new-board (assoc (:board game-state) position player)
           player-won (did-player-win? new-board player)]
-          (do (println player-won)
           (-> game-state (assoc :board new-board)
                          (assoc :game-status (if player-won :completed :in-progress))
-                         (assoc :solutions player-won)
-                  ))))
+                         (assoc :winner (if player-won player nil))
+                         (assoc :solutions player-won))))
 
 (defn is-board-full
   "Returns game-state with status set to :completed if the board is full"
@@ -75,8 +73,8 @@
     game-state
     )
 )
-
-(defn player-clicked
+(comment
+(defn player-clicked2
   "Do everything after player clicked:
    * players move
    * check if he won
@@ -88,25 +86,44 @@
   [game-state position]
   (let [players-turn (do-move game-state position :cross)
         board-full-after-player (is-board-full players-turn)
-        computer-turn (do-move board-full-after-player (computer-move board-full-after-player) :circle)
+        computer-turn (do-move board-full-after-player (computer-move (:board board-full-after-player)) :circle)
         board-full-after-computer (is-board-full computer-turn)
         ]
         (reduce #(if (= :completed (:game-status %1)) %1 %2) 
           (list players-turn board-full-after-player computer-turn board-full-after-computer))
-        ))
+        )))
 
+(defn player-clicked
+  "Do everything after player clicked:
+   * players move
+   * check if he won
+   * check if board is full
+   * computer move
+   * check if it won
+   * check if board is full
+  "
+  [game-state position]
+      (reduce #(if (= :completed (:game-status %1)) %1 (%2 %1)) 
+        [ (do-move game-state position :cross) ;player's turn
+          is-board-full ;check if board is full
+          (fn [x](do-move x (computer-move (:board x)) :circle)) ;computer's move
+          is-board-full ;check if board is full
+        ]))
+
+(defn svg-ref [id]
+  [:svg 
+    {:dangerouslySetInnerHTML
+      {:__html (str "<use xlink:href=\"#" id "\"></use>")}}])
 
 (defn cell-component [key] 
   (let [value (get (:board @game-state) key)
         solution? (some #{key} (:solutions @game-state))
         can-play (and (nil? value) (= :in-progress (:game-status @game-state)))
         ]
-    (do (println solution?)
-
-    ^{:key key}[:div.max-size
+    [:div.cell
       (if can-play
         {:on-click #(let [n (player-clicked @game-state key)]
-          (do (println "fokot " n)
+          (do (println "game: " n)
           ;(swap! game-state player-clicked key)
           (reset! game-state n)
           ))}
@@ -116,40 +133,34 @@
       
       (case value
         nil ""
-        :cross "\u274C"
-        :circle "\u2B55"
-      )])))
-
-
-
-(defn board-component [] 
-  [:table
-    [:tbody
-    (for [x [0 1 2]]
-      [:tr
-        (for [y [0 1 2]]
-          [:td
-            [cell-component [x y]]
-          ]
-        )
-      ]
-    )
-    ]])
+        :cross [svg-ref "cross"]
+        :circle [svg-ref "circle"]
+      )]))
 
 (defn winner-annoucement []
   (when (= :completed (:game-status @game-state))
-    [:div "compeleted"]
-    )
-  )
+    [:div.announcement (case (:winner @game-state)
+           :cross "You won!!!"
+           :circle "Computer won!!!"
+           nil "Cat's game!!!")]))
 
-(defn start-new-game-component [] 
-  [:button {:on-click #(reset! game-state new-game-state)}])
+(defn board-component [] 
+  [:div {:id "board"}
+    (for [x [0 1 2]]
+        (for [y [0 1 2]]
+            [cell-component [x y]]))
+    [winner-annoucement]])
 
-(defn game-component [] [:div
-  [board-component]
-  [start-new-game-component]
-  [winner-annoucement]
-  ])
+(defn restart-game-component [] 
+  [:button
+    {:on-click #(reset! game-state new-game-state)}
+    "New game"])
+
+(defn game-component []   
+  [:div {:id "game"}
+    [:h1 "Ultimate tic-tac-toe"]
+    [board-component]
+    [restart-game-component]])
 
 ;; Render the root component
 (defn start []
